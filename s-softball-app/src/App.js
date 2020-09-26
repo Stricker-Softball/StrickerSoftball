406045cfeacc7d2a8c518b80215c025a66997798
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-09-25 19:26:46 -0500

    imported components and changed styles

--- s-softball-app/src/App.js
+++ s-softball-app/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Navbar from './components/Navbar/Navbar.js'
+import Home from './components/Home/Home.js';
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Home />
 
     </div>
   );
