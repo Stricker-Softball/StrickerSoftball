858423e4d42153b1814906c570823836bf0bf1e3
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-09-25 19:27:35 -0500

    template and style navbar

--- s-softball-app/src/components/Navbar/Navbar.js
+++ s-softball-app/src/components/Navbar/Navbar.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
 import './Navbar.css';
 
 function Navbar() {
     return (
         <div className="Navbar">
+            <nav className={'Navbar-items'}>
+                <NavLink to='/'>
+                    Home
+                </NavLink>
+                <NavLink to='/softballlessons'>
+                    Softball Lessons
+                </NavLink>
+                <NavLink to='/wphg'>
+                    Weekly Power Hitting Groups
+                </NavLink>
+                <NavLink to='/bookonline'>
+                    Book Online
+                </NavLink>
+                <NavLink to='/meetthecoaches'>
+                    Meet The Coaches
+                </NavLink>
+                <NavLink to='/coach2coach'>
+                    Coach 2 Coach
+                </NavLink>
+            </nav>
 
         </div>
     );
