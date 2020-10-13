e5134402cc550948ea993dea46f6ad607cfc03d2
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-10-13 15:02:08 -0500

    images and heaer added to home

--- s-softball-app/src/components/Home/Home.js
+++ s-softball-app/src/components/Home/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Home.css';
 
+import EqupmentPile from '../../images/equipment-pile.jpg';
+import MitGlove from '../../images/mit-glove.jpg';
+
 function Home() {
     return (
         <div className="Home">
-
+            <h1>Stricker Softball</h1>
+            <div className='images' >
+                <img src={EqupmentPile} />
+                <img src={MitGlove} />
+            </div>
         </div>
     );
 }
