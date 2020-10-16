d7ba65ad1d2e763d77706da66280ff3793045cb4
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-10-16 16:47:47 -0500

    footer structure, basic styling

---
+++ s-softball-app/src/components/Footer/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import './Footer.css';
+
+import FacebookIcon from '../../images/social/facebook.png';
+import instaIcon from '../../images/social/instagram.png';
+import twitterIcon from '../../images/social/twitter.png';
+
+function Footer() {
+    return (
+        <div className="Footer">
+            <div className='footer-section'>
+                <div className='sub-section subscribe'>
+                    <h4>Subscribe!</h4>
+                    <div className='sub-input'>
+                        <input />
+                        <button>Sign Up</button>
+                    </div>
+                </div>
+                <div className='sub-section info'>
+                    <p>Based out of <span>Geneva, IL</span> with lessons At <span>Bring It! Sports</span></p>
+                    <span>303 N. 4th Street St. Charles</span>
+                </div>
+            </div>
+            <div className='footer-section'>
+                <div className='sub-section copyright'>
+                    <span>©2018 by Building 360 Performance</span>
+                </div>
+                <div className='sub-section social'>
+                    <div className='social-icons'>
+                        <img src={FacebookIcon} alt='facebook' />
+                        <img src={instaIcon} alt='instagram' />
+                        <img src={twitterIcon} alt='twitter' />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Footer;
