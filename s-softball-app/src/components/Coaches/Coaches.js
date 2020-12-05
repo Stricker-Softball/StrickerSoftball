e501dc19744c36d6376d9728a48838afc94f4f04
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-12-04 21:56:13 -0600

    coahces html fleshed out, images added, and popup functionality

--- s-softball-app/src/components/Coaches/Coaches.js
+++ s-softball-app/src/components/Coaches/Coaches.js
@@ -1,30 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Coaches.css';
+
+import SuePortriat from '../../images/coaches/sue-portrait.png';
+import MegPortrait from '../../images/coaches/megan-portrait.png';
+import JenaPortrait from '../../images/coaches/jennah-portrait.png';
+import JenyPortrait from '../../images/coaches/jenny-portrait.png';
+import AllyPortrait from '../../images/coaches/ally-portrait.png';
+import DotPortrait from '../../images/coaches/dot-portrait.png';
+
+
 
 
 function Coaches() {
+    const [popup, setPopup] = useState(false);
+    let coaches = [
+        { name: 'coach sue', image: SuePortriat, color: 'red' },
+        { name: 'coach megan', image: MegPortrait, color: 'blue' },
+        { name: 'coach Jennah', image: JenaPortrait, color: 'blue' },
+        { name: 'coach Jenny', image: JenyPortrait, color: 'red' },
+        { name: 'coach ally', image: AllyPortrait, color: 'red' },
+        { name: 'coach Dot', image: DotPortrait, color: 'blue' }
+    ]
+
+    function handlePopup() {
+        if (popup == false) {
+            setPopup(true)
+        } else {
+            setPopup(false)
+        }
+    }
+
     return (
-        <div className="Coaches">
-            <h1>Changing the game one player at a time</h1>
-            <div className='coach-container'>
-                <div className='coach-sectoin'>
-                    <div className='coach-card'>
-                        <h2 className='name'>Coach Sue</h2>
-                        <img />
-                        <button className='button-main'>Profile</button>
-                    </div>
-                    <div className='coach-card'></div>
-                </div>
-                <div className='coach-sectoin'>
-                    <div className='coach-card'></div>
-                    <div className='coach-card'></div>
-                </div>
-                <div className='coach-sectoin'>
-                    <div className='coach-card'></div>
-                    <div className='coach-card'></div>
+
+        <>
+            <div className={popup ? 'popup-main' : 'popup-main deactive'}>
+                <div onClick={() => handlePopup()} className='popup-window'>
+
                 </div>
             </div>
-        </div>
+            <div className="Coaches">
+                <h1>- Changing the game one player at a time -</h1>
+                <div className='coach-container'>
+                    {coaches.map((coach, index) => {
+                        return (
+                            <div className={`${coach.color === 'blue' ? 'coach-card card-2nd' : 'coach-card'}`}>
+                                <h2 className='name'>{coach.name.toUpperCase()}</h2>
+                                <div className='img-div'>
+                                    <div className='img-overflow'>
+                                        <img src={coach.image} alt={coach.name} />
+                                    </div>
+                                </div>
+                                <div className='btn-div'>
+                                    <button onClick={() => handlePopup()} className='button-main'>Profile</button>
+                                </div>
+                            </div>
+                        )
+                    })}
+
+                </div>
+                <div>
+                    <h1>Do today what others won't so tomorrow you can do what others can't.</h1>
+                    <h1>- Jerry Rice</h1>
+                </div>
+            </div>
+        </>
     );
 }
 
