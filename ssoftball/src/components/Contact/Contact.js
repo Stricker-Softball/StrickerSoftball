996ac007973faa762a9d22a2fbdbe985f529cf7e
Nick Stricker <strickerdev@gmail.com>
2020-12-30 16:26:23 -0600

    contact html organization

--- ssoftball/src/components/Contact/Contact.js
+++ ssoftball/src/components/Contact/Contact.js
@@ -11,12 +11,12 @@ function Contact() {
             <h2>Contact Us</h2>
             <div className='contact-section'>
                 <div className='contact-info'>
-                    <span className='label'>Send an email:</span>
-                    <p>sftbllcoach@gmail.com</p>
+                    <p className='label'>Send an email:</p>
+                    <p className='data'>sftbllcoach@gmail.com</p>
                 </div>
                 <div className='contact-info'>
-                    <span className='label'>Call or Text:</span>
-                    <p>630-336-1553</p>
+                    <p className='label'>Call or Text:</p>
+                    <p className='data'>630-336-1553</p>
                 </div>
             </div>
 
