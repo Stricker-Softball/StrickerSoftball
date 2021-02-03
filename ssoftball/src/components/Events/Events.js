c57c37aab00cd57c34b7bec9c4fd097da2197ef0
Nick Stricker <strickerdev@gmail.com>
2021-02-02 21:23:19 -0600

    api events added

--- ssoftball/src/components/Events/Events.js
+++ ssoftball/src/components/Events/Events.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './Events.scss';
 
 import SpringEvent from '../../images/pdf/SpringBreak2021FundamentalsCamps.jpg';
@@ -12,16 +13,25 @@ import SpringEvent from '../../images/pdf/SpringBreak2021FundamentalsCamps.jpg';
 function Events(props) {
     // const [numPages, setNumPages] = useState(null);
     // const [pageNumber, setPageNumber] = useState(1);
+    const [events, setEvents] = useState([]);
 
-    // useEffect(() => {
+    useEffect(() => {
+        async function fetchData() {
+            const result = await axios.get(
+                'https://stricker-softball.herokuapp.com/api/events/'
+            )
+            setEvents(result.data);
+          }
+          fetchData();
+        }, []);
         
-    // })
+
    
     // function onDocumentLoadSuccess({ numPages }) {
     //     console.log('-------------docuntment loaded!!!')
     //      setNumPages(numPages);
     // }
-  
+    console.log(events)
     return (
     //   <div className='Events'>
     //       <p> events</p>
@@ -36,6 +46,21 @@ function Events(props) {
         <h1>Events</h1>
         
         <img src={SpringEvent} alt='springevent'/>
+        
+        <div className='event-list'>
+            {events.map((item) =>{
+                return (
+                    <div className='event-item'>
+                        <h3>{item.title}</h3>
+                        <div className='event-body'>{item.body}</div>
+                    </div>)
+            })}
+        </div>
+
+        <p onClick={() =>{
+        window.scrollTo(0,0)
+        props.history.push('/admin/events/edit')   
+        }}>ed</p>
     </div>
     );
 }
