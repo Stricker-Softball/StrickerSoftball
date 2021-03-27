6b0e9b75d08f484a1348e6b9b199b5eec60583bf
Nick Stricker <strickerdev@gmail.com>
2021-03-26 21:07:10 -0500

    events page made to display pdf's

--- ssoftball/src/components/Events/Events.js
+++ ssoftball/src/components/Events/Events.js
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Events.scss';
-
-import SpringEvent from '../../images/pdf/SpringBreak2021FundamentalsCamps.jpg';
 // import { Document, Page, pdfjs  } from 'react-pdf';
 
 
 // pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
+import sbllAcademyMemberships from '../../images/pdf/SoftballAcademyMemberships.pdf';
+import sbllAcademy from '../../images/pdf/StrickerSoftballAcademy.pdf';
+import sbllAprilMay from '../../images/pdf/StrickerSoftballAcademyAprilMay.pdf';
+import sbllSummer from '../../images/pdf/StrickerSoftballAcademySummer.pdf';
 
 
 function Events(props) {
     const [events, setEvents] = useState(props.eventList || []);
+    const [eventImages, setEventImage] = useState([
+        sbllAcademy,
+        sbllAcademyMemberships,
+        sbllAprilMay,
+        sbllSummer
+    ])
 
     useEffect(() => {
         async function fetchData() {
@@ -39,6 +47,14 @@ function Events(props) {
                             <h3>{item.title}</h3>
                             <div className='event-body'>{item.body}</div>
                         </div>)
+                })}
+
+                {eventImages.map((item) => {
+                    return (
+                        <div className='event-item' style={{paddingBottom:'0'}}>
+                            <embed src={item} width="100%" height="800px" />
+                        </div>
+                    )
                 })}
             </div>
 
