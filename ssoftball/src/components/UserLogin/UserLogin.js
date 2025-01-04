d71b72302b023e52759d9f9644d2b6d7a2af8958
Nick Stricker <strickerdev@gmail.com>
2025-01-03 20:01:48 -0600

    Softball sience update styling

--- ssoftball/src/components/UserLogin/UserLogin.js
+++ ssoftball/src/components/UserLogin/UserLogin.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLock } from '@fortawesome/free-solid-svg-icons'
 
 function AdminScreen(props) {
     let defaultPassword = 'ss2025'
@@ -10,7 +12,6 @@ function AdminScreen(props) {
     const handleSubmit = (e) => {
         e.preventDefault()
         let formData = new FormData(e.target)
-        console.log('submit')
         for (const pair of formData.entries()) {
             if(pair[0] == 'password') {
                 if(pair[1] == defaultPassword){
@@ -36,13 +37,16 @@ function AdminScreen(props) {
                 {/* <button onClick={handleCancel} className="adminFormButton modalCloseBtn">Close</button> */}
                 <form id="userLoginModalForm" onSubmit={handleSubmit} >
                     <div className="formGroup">
-                        <label>Enter a Code to view The Stations Page</label>
+                    <FontAwesomeIcon icon={faLock} className="modal-icon"/>
+                        <label>
+                            Enter a Code to view this page - ( Ask a coach )</label>
                         <input type="password" name="password" data-name="password" />
                     </div>
                     
                     <div className="modal-footer">
-                        <button type="submit" className="adminFormButton">Submit</button>
+                        
                         <button onClick={handleCancel} className="adminFormButton">Cancel</button>
+                        <button type="submit" className="adminFormButton submitButton">Enter</button>
                     </div>
                 </form>
             </div>
